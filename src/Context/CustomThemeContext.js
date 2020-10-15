7ad2298286a1a61423450144aeac919014e1ceea
AlexMachin1997 <AlexMachin1997@gmail.com>
2020-10-15 16:42:42 +0100

    Upgrade the Settings screen to use the context api and sync the values to storage

--- src/Context/CustomThemeContext.js
+++ src/Context/CustomThemeContext.js
@@ -1,9 +1,28 @@
-import React, {useContext, createContext, useState} from 'react';
+import React, {useContext, createContext, useState, useEffect} from 'react';
+import { getItem, setItem } from '../Utils/AsyncStorage';
 
 const CustomThemeContext = createContext();
 
 export const CustomThemeProvider = ({children}) => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(null);
+  
+
+  useEffect(() => {
+    // Get the shopping lists from storage 
+    const storage = async () => {
+      // Get the items
+      if(isDark === null) {
+        const res = await getItem('isDark');
+        setIsDark(res);
+      } else {
+        await setItem('isDark', isDark);
+        setIsDark(isDark);
+      }
+    }
+
+    storage();
+
+  }, [isDark])
 
   return (
     <CustomThemeContext.Provider value={[isDark, setIsDark]}>
